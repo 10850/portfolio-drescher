bacb8212eab6f5eaa821c4050d5b1068cb967979
10850 <10850@student.eadania.dk>
2020-12-15 15:04:33 +0100

    styling og grids
    har stylet elementerne og skal til desktop grid

--- design-thinking/designthinking.php
+++ design-thinking/designthinking.php
@@ -22,6 +22,10 @@
         
         </header>
             <div id="mitgrid-designthinking">
+                
+                <h2 class="designthinking-overskrift">Design Thinking</h2>
+                <img class="designthinking-img" id="designthinking-faser" src="designthinking-faser.png">
+                
                 <section class="hvaderdesignthinking">
                     <h2 class="titel-designthinking">
                         Hvad er Design Thinking?
@@ -36,74 +40,89 @@
 
 
                 <section class="empathize">
-                    <h2 class="titel-designthinking">
-                        Empathize
-                    </h2>
+                    <div>
+                        <h3 class="titel-designthinking" id="titel-empathize">
+                            Empathize
+                        </h3>
+
+                        <img src="empathize-fase-ikon.png" class="fase-ikoner" id="empathize-ikon">
+                    </div> <!-- denne div er til at for at titel og ikon sidder sammen i grid for desktop -->
+                    <img class="designthinking-img" src="interview-empathize.jpg" id="billed-empathize">
                     
-                    <img src="interview-empathize.jpg">
-                    
-                    <p class="tekst-designthinking">
+                    <p class="tekst-designthinking" id="tekst-empathize">
                         Empathize er den første fase og er her man skal være nysgerrig, man tager det firma, produkt eller bare emne man har at gøre med og researcher efter så meget information man kan ved at stille en masse nysgerrige spørgsmål til emnet. Man kan researche på nettet, læse i bøger eller artikler eller gå ud og interviewe selve målgruppen for et mere menneskeligt indblik på tingene. Denne proces har været den proces som altid skulle bruges mest tid på og sætte mig op til at kunne definere det egentlige problemer jeg skal løse.
                     </p>
                 </section> <!-- Empathize -->
 
 
                 <section class="define">
-                    <h2 class="titel-designthinking">
-                        Define
-                    </h2>
+                    <div>
+                        <h3 class="titel-designthinking" id="titel-define">
+                            Define
+                        </h3>
+
+                        <img src="define-fase-ikon.png" class="fase-ikoner" id="define-ikon">
+                    </div> <!-- denne div er til at for at titel og ikon sidder sammen i grid for desktop -->
+                    <img class="designthinking-img" src="hackathon-define.jpg" id="billed-define">
                     
-                    <img src="hackathon-define.jpg">
-                    
-                    <p class="tekst-designthinking">
+                    <p class="tekst-designthinking" id="tekst-define">
                         Define bygger videre på alt den information man har fået gennem empathize ved at gøre den store mængde af information som er så bredt til noget mere konkret og ligetil at arbejde videre med. Det gør vi mange gange ved at kigge på hvad der typisk så ud til at være målgruppens problem med noget eller virksomhedens mål med noget i forhold til hvor de er nu. Derved kan man finde frem til en form for problemstilling som man kan begynde at få ideer til at finde den mest optimale løsning til. Dog skal man huske at siden design thinking er en iterativ proces at man skal genbesøge sine problemformuleringer i det man får mere viden og kontekst, som kan ende ud i en problemformulering som passer bedre til hvad problemet egentligt er.
                     </p>
                 </section> <!-- Define -->
 
 
                 <section class="ideate">
-                    <h2 class="titel-designthinking">
-                        Ideate
-                    </h2>
+                    <div>
+                        <h3 class="titel-designthinking" id="titel-ideate">
+                            Ideate
+                        </h3>
+
+                        <img src="ideate-fase-ikon.png" class="fase-ikoner" id="ideate-ikon">
+                    </div> <!-- denne div er til at for at titel og ikon sidder sammen i grid for desktop -->
+                    <img class="designthinking-img" src="hackathon-ideate.jpg" id="billed-ideate">
                     
-                    <img src="hackathon-ideate.jpg">
-                    
-                    <p class="tekst-designthinking">
+                    <p class="tekst-designthinking" id="tekst-ideate">
                          Ideate er der hvor det ”sjove” begynder og man kan få lov til at åbne for døren til sin hjerne og sige alle de ideer som kommer en i tankerne. Dette er en iterativ process hvor man helst ville gå igennem en 3 rundte af brainstorm. Første runde hvor man får alle de simple løsninger skrevet ned på labels og hangt op på tavlen. Anden rundte hvor der ville komme mere innovative ideer som måske kombinerer nogle ideer der allerede er bragt op. Og til sidst den tredje rundte hvor man virkelig føler man er tømt for ideer, men så lige tiden er ved at slutte springer en vild ide man aldrig ville tro man ville komme op med. Det vigtigste i ideate når man går igennem sin brainstorm er at huske at der ikke findes en ”dårlig ide” alle ideer er velkommende. Derefter kan man begynde at evaluere ideerne og finde den/de mest optimale og innovative ideer til at arbejde videre med.  Det har hjulpet os mange gange til at finde frem til ideer som ingen af os ville kunne gøre alene fordi det altid ender med en kombination af alles ideer. Det er også sket at man får en ny ide efter ideate fasen som man kunne bruge. Faktisk er det sket hver eneste gang.
                     </p>
                 </section> <!-- Ideate -->
 
 
                 <section class="prototype">
-                    <h2 class="titel-designthinking">
-                        Pototype + Test
-                    </h2>
+                    <div>
+                        <h3 class="titel-designthinking" id="titel-prototype">
+                            Pototype + Test
+                        </h3>
+
+                        <img src="prototype-fase-ikon.png" class="fase-ikoner" id="prototype-ikon">
+                    </div> <!-- denne div er til at for at titel og ikon sidder sammen i grid for desktop -->
                     
-                    <img src="hackathon-prototype.jpg">
+                    <img class="designthinking-img" src="hackathon-prototype.jpg" id="billed-prototype">
                     
-                    <p class="tekst-designthinking">
+                    <p class="tekst-designthinking" id="tekst-prototype">
                         Nu kommer man til prototype og test hvor man skal lave en prototype af sin ide og få lavede nogle tests så det er man kan få feedback og lave om på prototypen før man laver det endelige produkt. Når man laver sin prototype, skal man lave den mest simple, billige og mindst tidskrævende prototype for at gøre fasen så let så mulig at udføre det er for os kommet hovedsageligt af wire frames på papir, som vi så tester, laver ændringer og tester igen. Derefter går vi videre til at lave en Hi-Fi-prototype på adobe XD, som kan illustrere det endelige design og funktionalitet af siden, næsten som om siden er færdig.
                     </p>
                 </section> <!-- Prototype + Test -->
 
 
                 <section class="implementering">
-                    <h2 class="titel-designthinking">
-                        Implementering
-                    </h2>
-                    
-                    
-                    
-                    <p class="tekst-designthinking">
+                    <div>
+                        <h3 class="titel-designthinking" id="titel-implement">
+                            Implementering
+                        </h3>
+
+                        <img src="implement-fase-ikon.png" class="fase-ikoner" id="implement-ikon">
+                    </div> <!-- denne div er til at for at titel og ikon sidder sammen i grid for desktop -->
+                    <p class="tekst-designthinking" id="tekst-implement">
                         Efter at have gået gennem alle faserne kommer man til at lave det endelige produkt, som typisk for os er et website, på HTML og CSS. Man gå ud fra sin Hi-Fi-prototype på XD som var lavede så kodningen ville blive så nem så muligt, fordi det simplethen er nemmere at kode noget når man bare skal kopier det der står lige foran en i stedet for at skulle finde ud af det on the fly.
                     </p>
                 </section> <!-- Implementering -->
                 
                 <section class="refleksion-designthinking">
-                    <h2 class="titel-designthinking">
+                    <h3 class="titel-designthinking">
                         Hvad jeg har fået ud af Design Thinking
-                    </h2>
+                    </h3>
                     
+                    <img class="designthinking-img" id="hackathon-medal" src="hackathon-medal.jpg">  
                     
                     
                     <p class="tekst-designthinking">
