63dfb641e19d0fe71de23d3b8333cc7ece331afe
10850 <10850@student.eadania.dk>
2020-12-15 12:53:59 +0100

    design thinking begyndt
    alt indhold er ved at blive sat ind for design thinking

--- design-thinking/designthinking.php
+++ design-thinking/designthinking.php
@@ -15,10 +15,105 @@
     <!-- Bootstrap stylesheet -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!-- Mit stylesheet -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
     <body class="designthinking-php">
+        <header>
+        
+        </header>
+            <div id="mitgrid-designthinking">
+                <section class="hvaderdesignthinking">
+                    <h2 class="titel-designthinking">
+                        Hvad er Design Thinking?
+                    </h2>
+                    
+                    
+                    
+                    <p class="tekst-designthinking">
+                        Design thinking er en tilgang til hvordan man skal tænke og bære sig ad med at løse et problem. Den sætter grund for at man gør sig nysgerrig før man begynder at få gode ideer og åbner op for at man kan få så mange ideer ud som muligt. Design thinking er en iterativ proces som består af fem faser, og er en proces som jeg har lært at bruge meget på studiet.
+                    </p>
+                </section> <!-- Hvad er design thinking -->
 
+
+                <section class="empathize">
+                    <h2 class="titel-designthinking">
+                        Empathize
+                    </h2>
+                    
+                    
+                    
+                    <p class="tekst-designthinking">
+                        Empathize er den første fase og er her man skal være nysgerrig, man tager det firma, produkt eller bare emne man har at gøre med og researcher efter så meget information man kan ved at stille en masse nysgerrige spørgsmål til emnet. Man kan researche på nettet, læse i bøger eller artikler eller gå ud og interviewe selve målgruppen for et mere menneskeligt indblik på tingene. Denne proces har været den proces som altid skulle bruges mest tid på og sætte mig op til at kunne definere det egentlige problemer jeg skal løse.
+                    </p>
+                </section> <!-- Empathize -->
+
+
+                <section class="define">
+                    <h2 class="titel-designthinking">
+                        Define
+                    </h2>
+                    
+                    
+                    
+                    <p class="tekst-designthinking">
+                        Define bygger videre på alt den information man har fået gennem empathize ved at gøre den store mængde af information som er så bredt til noget mere konkret og ligetil at arbejde videre med. Det gør vi mange gange ved at kigge på hvad der typisk så ud til at være målgruppens problem med noget eller virksomhedens mål med noget i forhold til hvor de er nu. Derved kan man finde frem til en form for problemstilling som man kan begynde at få ideer til at finde den mest optimale løsning til. Dog skal man huske at siden design thinking er en iterativ proces at man skal genbesøge sine problemformuleringer i det man får mere viden og kontekst, som kan ende ud i en problemformulering som passer bedre til hvad problemet egentligt er.
+                    </p>
+                </section> <!-- Define -->
+
+
+                <section class="ideate">
+                    <h2 class="titel-designthinking">
+                        Ideate
+                    </h2>
+                    
+                    
+                    
+                    <p class="tekst-designthinking">
+                         Ideate er der hvor det ”sjove” begynder og man kan få lov til at åbne for døren til sin hjerne og sige alle de ideer som kommer en i tankerne. Dette er en iterativ process hvor man helst ville gå igennem en 3 rundte af brainstorm. Første runde hvor man får alle de simple løsninger skrevet ned på labels og hangt op på tavlen. Anden rundte hvor der ville komme mere innovative ideer som måske kombinerer nogle ideer der allerede er bragt op. Og til sidst den tredje rundte hvor man virkelig føler man er tømt for ideer, men så lige tiden er ved at slutte springer en vild ide man aldrig ville tro man ville komme op med. Det vigtigste i ideate når man går igennem sin brainstorm er at huske at der ikke findes en ”dårlig ide” alle ideer er velkommende. Derefter kan man begynde at evaluere ideerne og finde den/de mest optimale og innovative ideer til at arbejde videre med.  Det har hjulpet os mange gange til at finde frem til ideer som ingen af os ville kunne gøre alene fordi det altid ender med en kombination af alles ideer. Det er også sket at man får en ny ide efter ideate fasen som man kunne bruge. Faktisk er det sket hver eneste gang.
+                    </p>
+                </section> <!-- Ideate -->
+
+
+                <section class="prototype">
+                    <h2 class="titel-designthinking">
+                        Pototype + Test
+                    </h2>
+                    
+                    
+                    
+                    <p class="tekst-designthinking">
+                        Nu kommer man til prototype og test hvor man skal lave en prototype af sin ide og få lavede nogle tests så det er man kan få feedback og lave om på prototypen før man laver det endelige produkt. Når man laver sin prototype, skal man lave den mest simple, billige og mindst tidskrævende prototype for at gøre fasen så let så mulig at udføre det er for os kommet hovedsageligt af wire frames på papir, som vi så tester, laver ændringer og tester igen. Derefter går vi videre til at lave en Hi-Fi-prototype på adobe XD, som kan illustrere det endelige design og funktionalitet af siden, næsten som om siden er færdig.
+                    </p>
+                </section> <!-- Prototype + Test -->
+
+
+                <section class="implementering">
+                    <h2 class="titel-designthinking">
+                        Implementering
+                    </h2>
+                    
+                    
+                    
+                    <p class="tekst-designthinking">
+                        Efter at have gået gennem alle faserne kommer man til at lave det endelige produkt, som typisk for os er et website, på HTML og CSS. Man gå ud fra sin Hi-Fi-prototype på XD som var lavede så kodningen ville blive så nem så muligt, fordi det simplethen er nemmere at kode noget når man bare skal kopier det der står lige foran en i stedet for at skulle finde ud af det on the fly.
+                    </p>
+                </section> <!-- Implementering -->
+                
+                <section class="refleksion-designthinking">
+                    <h2 class="titel-designthinking">
+                        Hvad jeg har fået ud af Design Thinking
+                    </h2>
+                    
+                    
+                    
+                    <p class="tekst-designthinking">
+                        
+                    </p>
+                </section>
+            </div> <!-- MITGRID DESIGN THINKING -->
+        <footer>
+        
+        </footer>
         
         <!-- Bootstrap script -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
