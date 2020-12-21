29619c5331d3dfbc696223667dc373cb00b317cb
10850 <10850@student.eadania.dk>
2020-12-21 12:21:22 +0100

    fotografering start
    laver en ny branch til fotografering efter

--- fotografering.php
+++ fotografering.php
@@ -18,7 +18,14 @@
     <link rel="stylesheet" href="style.css">
 </head>
     <body class="foto-php">
-
+        <header>
+            <?php include 'menu.inc' ?>
+        </header>
+        
+        
+        
+        
+        <?php include 'footer.inc' ?> <!-- footer -->
         
         <!-- Bootstrap script -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
