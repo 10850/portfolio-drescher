554fef153c2e0fa5bf88a043e35719440b3b75f8
10850 <10850@student.eadania.dk>
2020-12-22 12:36:43 +0100

    usability
    en side til usability er færdig og skal til at igang med gangstertest siden. Dog stod jeg på main ved en fejl så skal lave ny branch igen.

--- gangstertest.php
+++ gangstertest.php
@@ -18,7 +18,130 @@
     <link rel="stylesheet" href="style.css">
 </head>
     <body class="gangstertest-php">
-
+        <header>
+            <?php include 'menu.inc' ?>
+        </header>
+        
+        <div id="mitgrid-gangstertest">
+            
+            <h1>Gangstertest</h1>
+            
+            <section class="hvad-er-gangstertest">
+                <h2 class="overskrift">Hvad er en gangstertest?</h2>
+                <p class="body-text">
+                    Usability betyder brugbarhed, det vil sige at ordet beskriver hvor nemt og flydende det føles for personen der bruger hjemmesiden (Gregersen &#38; Wisler-Poulsen, 2017). Usability handler også om at hjemmesiden eller programmet fungerer som de ønsker det, og for de typer af mennesker de har lavet hjemmesiden til (ibid).
+                </p>
+            </section> <!-- hvad er usability -->
+            
+            <section class="udforelse-gangstertest">
+                
+                <h2 class="overskrift">Udførelse</h2>
+                <ul class="hvorfor-liste">
+                    <li>Fordi usability er en cornerstone for websitets succes (Gregersen 	&#38; Wisler-Poulsen, 2017).</li>
+                    <li>Hvis man er ligeglad med usability, så er man også ligeglad med og folk bruger sin website (ibid).</li>
+                    <li>Det er hvad gøre det muligt at sende sit budskab ud og faktisk få en indflydelse hos folk (som man ønsker) (ibid).</li>
+                    <li>Hvis der ikke er usability af høj kvalitet så, falder kvaliteten af sitet, og virksomheden bag vil repræsenteres med et dårligt ry hos deres kunder (ibid).</li>
+                </ul>
+            </section> <!-- udførelse -->
+            
+            <section class="gangstertest-sporgsmal">
+                <h3 class="underoverskrift">Typiske spørgsmål</h3>
+                <p class="body-text">
+                    Det er behov for nem brugbarhed, som websitet opfylder for sine brugere, så websitet føles brugbart og ligetil for brugeren (Gregersen 	&#38; Wisler-Poulsen, 2017). Hvis brugerne ikke får sine behov opfyldt, kan det virke svært for brugeren at bruge siden og så vil de jo bare gå videre og finde side der er mere funktionelt (ibid).
+                </p>
+            </section> <!-- spørgsmål -->
+            
+            <section class="gangster-pointsystem">
+                <h3 class="underoverskrift">Pointsystem</h3>
+                <table class="usability-table">
+                    <tr>
+                        <th>Typisk fokusområde</th>
+                        <th>Særligt egnede testmetoder</th>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Navigation og struktur (informationsarkitektur)
+                        </td>
+                        <td>
+                            <p>Gangstertest (navigation)</p> 
+                            <p>Kortsortering (Informationsstruktur)</p> 
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Funktionalitet (interaktionsdesign)
+                        </td>
+                        <td>
+                            <p>Spørgeskema</p>
+                            <p>Ekspertvurdering</p>
+                            <p>Burgertest</p>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Layout og opbygning
+                        </td>
+                        <td>
+                            <p>Gangstertest</p>
+                            <p>Ekspertvurdering</p>
+                            <p>Brugertest</p>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Tilgængelighed
+                        </td>
+                        <td>
+                            <p>Ekspertvurdering</p>
+                            <p>Brugertest</p>
+                            <p>Tilgængelighedstest</p>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Forståelighed
+                        </td>
+                        <td>
+                            <p>Spørgeskema </p>
+                            <p>Kortsortering</p>
+                            <p>Brugertest</p>
+                        </td>
+                    </tr>
+                </table>
+            </section> <!-- pointsystem -->
+            
+            <section class="dokumentation-gangster">
+                <h2 class="overskrift">Dokumentation</h2>
+                <p class="body-text">
+                    Pointen i brugertilfredshed er at give borgerne fra de gældende kommuner en stemme hvad ang. det lokale udviklingsarbejde (Gregersen &#38; Wisler-Poulsen, 2017). Et eksempel kunne bl.a. være at spørge voksne over 18, der modtager bostøtte eller bor i boligfællesskaber hvad de synes om deres tilbud (ibid).
+                </p>
+            </section> <!-- dokumentation -->
+            
+            <section class="gangstertest-rapport">
+                <h2 class="overskrift">Gangstertest på Youtube</h2>
+                <p class="body-text">
+                    Undersøgelsesdesign refererer til den overordnede ramme for indsamling og analyse af data (Gregersen &#38; Wisler-Poulsen, 2017). Valget af undersøgelsesdesign siger noget om forskerens refleksioner over prioriteringen af forskellige aspekter af undersøgelsesprocessen, f.eks. kausalitet og generalisering (ibid).
+                </p>
+                <p class="body-text">
+                    Undersøgelsesdesign refererer til formålet og fokusområdet/-erne som ens undersøgelse har (ibid). Her går det ind på valget af metoder og deres fokusområde. Indenfor usability er det vigtigt at man vælger flere metoder med forskellige synsvinkler for hvis man kun vælger en metode at teste med eller flere test som kun dækker et fokusområde vil ens løsning til et problem bliver alt for snævret og være baseret på et skrøbeligt fundament (ibid). Derfor er det en anbefaling at benytter sig af metodetriangulering og vælger flere forskellige metoder. Det giver nemlig det bedst mulige vurderingsgrundlag (ibid).
+                </p>
+            </section> <!-- praktisk eksempel fra rapport -->
+            
+            <section class="kilder">
+                <h2 class="overskrift">Kilder</h2>
+                <ul class="kildeliste">
+                    <li>Gregersen, O. &#38; Wisler-Poulsen, I., 2017. Hvad er Usability?. I: Usability - Testmetoder til mere brugbare websites. s.l.:Wislers Forlag, pp. 14-24.</li>
+                    <li>Gregersen, O. &#38; Wisler-Poulsen, I., 2017. Undersøgelsesdesign. I: Usability - Testmetoder til mere brugbare websites. s.l.:Wislers Forlag, pp. 24-42.</li>
+                </ul>
+            </section>
+        </div>
+        
+        <?php include 'footer.inc' ?> <!-- footer -->
         
         <!-- Bootstrap script -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
--- hvad-er-usability.php
+++ hvad-er-usability.php
@@ -18,7 +18,167 @@
     <link rel="stylesheet" href="style.css">
 </head>
     <body class="hvad-er-usability-php">
+        
+        <header>
+            <?php include 'menu.inc' ?>
+        </header>
+        
+        <div id="mitgrid-hvaderusability">
+            
+            <h1>Usability</h1>
+            
+            <section class="hvad-er-usability">
+                <h2 class="overskrift">Hvad er usability?</h2>
+                <p class="body-text">
+                    Usability betyder brugbarhed, det vil sige at ordet beskriver hvor nemt og flydende det føles for personen der bruger hjemmesiden (Gregersen &#38; Wisler-Poulsen, 2017). Usability handler også om at hjemmesiden eller programmet fungerer som de ønsker det, og for de typer af mennesker de har lavet hjemmesiden til (ibid).
+                </p>
+            </section> <!-- hvad er usability -->
+            
+            <section class="hvorfor-usability">
+                <h2 class="overskrift">Hvorfor usability?</h2>
+                <ul class="hvorfor-liste">
+                    <li>Fordi usability er en cornerstone for websitets succes (Gregersen 	&#38; Wisler-Poulsen, 2017).</li>
+                    <li>Hvis man er ligeglad med usability, så er man også ligeglad med og folk bruger sin website (ibid).</li>
+                    <li>Det er hvad gøre det muligt at sende sit budskab ud og faktisk få en indflydelse hos folk (som man ønsker) (ibid).</li>
+                    <li>Hvis der ikke er usability af høj kvalitet så, falder kvaliteten af sitet, og virksomheden bag vil repræsenteres med et dårligt ry hos deres kunder (ibid).</li>
+                </ul>
+            </section> <!-- hvorfor usability -->
+            
+            <section class="funktionalitet">
+                <h2 class="overskrift">Funktionalitet</h2>
+                <p class="body-text">
+                    Det er behov for nem brugbarhed, som websitet opfylder for sine brugere, så websitet føles brugbart og ligetil for brugeren (Gregersen 	&#38; Wisler-Poulsen, 2017). Hvis brugerne ikke får sine behov opfyldt, kan det virke svært for brugeren at bruge siden og så vil de jo bare gå videre og finde side der er mere funktionelt (ibid).
+                </p>
+            </section> <!-- funktionalitet -->
+            
+            <section class="effektivitet">
+                <h2 class="overskrift">Effektivitet</h2>
+                <p class="body-text">
+                    Effektivitet er behovet for at finde det som er hurtigt, nemt og ligetil. Det er så essentielt at man prioriterer at siden skal være så simpel at bruge så muligt ved brug af konventioner (Gregersen &#38; Wisler-Poulsen, 2017). Det er sin egen konkurrenceparameter, som siger at hvis dit site ikke kan konkurrere med de andres effektivitet, så vil folk gå til de andre sider i stedet for din (ibid).
+                </p>
+            </section> <!-- effektivitet -->
+            
+            <section class="tilfredshed">
+                <h2 class="overskrift">Tilfredshed</h2>
+                <p class="body-text">
+                    Pointen i brugertilfredshed er at give borgerne fra de gældende kommuner en stemme hvad ang. det lokale udviklingsarbejde (Gregersen &#38; Wisler-Poulsen, 2017). Et eksempel kunne bl.a. være at spørge voksne over 18, der modtager bostøtte eller bor i boligfællesskaber hvad de synes om deres tilbud (ibid).
+                </p>
+            </section> <!-- tilfredshed -->
+            
+            <section class="undersogelsesdesign">
+                <h2 class="overskrift">Hvad er et undersøgelsesdesign?</h2>
+                <p class="body-text">
+                    Undersøgelsesdesign refererer til den overordnede ramme for indsamling og analyse af data (Gregersen &#38; Wisler-Poulsen, 2017). Valget af undersøgelsesdesign siger noget om forskerens refleksioner over prioriteringen af forskellige aspekter af undersøgelsesprocessen, f.eks. kausalitet og generalisering (ibid).
+                </p>
+                <p class="body-text">
+                    Undersøgelsesdesign refererer til formålet og fokusområdet/-erne som ens undersøgelse har (ibid). Her går det ind på valget af metoder og deres fokusområde. Indenfor usability er det vigtigt at man vælger flere metoder med forskellige synsvinkler for hvis man kun vælger en metode at teste med eller flere test som kun dækker et fokusområde vil ens løsning til et problem bliver alt for snævret og være baseret på et skrøbeligt fundament (ibid). Derfor er det en anbefaling at benytter sig af metodetriangulering og vælger flere forskellige metoder. Det giver nemlig det bedst mulige vurderingsgrundlag (ibid).
+                </p>
+            </section> <!-- undersøgelsesdesign -->
+            
+            <section class="pilottest">
+                <h2 class="overskrift">Pilottest</h2>
+                <p class="body-text">
+                    Pilottesten er kendt som en test af testen og er altid anbefalet til en hver form for test man har at gøre med (Gregersen &#38; Wisler-Poulsen, 2017). Den har til formål at teste om en testmetode forløber efter hensigten, eller om den skal justeres (ibid). Det er vigtigt fordi en test er en unik situation som kun kan udføres en gang med hver person (ibid). For hvis de allerede har været igennem testen og den ender med ikke at være af god nok kvalitet, så vil man ikke kunne testen den samme person igen da deres holdninger og adfærd vil ændre sig på andet forsøg (ibid).
+                </p>
+            </section> <!-- pilottest -->
+            
+            <section class="lige-po-og-rot">
+                <h2 class="overskrift">Hvad er ”Lige på og råt”?</h2>
+                <p class="body-text">
+                    Man ville kalde denne metode/tilgang for ”Lige på og Råt” (Gregersen &#38; Wisler-Poulsen, 2017). Lige på og råt er en tilgang til at teste, vis formål er at lave hurtige og billige test som på samme tid er effektive når det kommer til indsamling af information (ibid). 
+                </p>
+                <p class="body-text">
+                    Det er en rigtig god metode til når man er på et budget og/eller gerne vil spare på sine ressourcer til når man faktisk skal udvikle produktet (ibid).
+                </p>
+            </section> <!-- Lige på og råt -->
+            
+            <img src="design-thinking/IMG_20201008_113001.jpg" alt="Lige på og råt prototype fra design thinking projektet" class="skitse-madheltene">
 
+            
+            <section class="skitster">
+                <h2 class="overskrift">Skitser og prototyper</h2>
+                <p class="body-text">
+                    Skitser kan være en tegning af en persons forestilling af det de vil udvikle eller videreudvikle på (Gregersen &#38; Wisler-Poulsen, 2017). Den er simpel at lave og man har ikke noget imod at tage chancer da man nemt kan erstattes eller blive lavet igen (ibid).
+                </p>
+                <p class="body-text">
+                    Prototyper er hvor man laver et produkt i lavere kvalitet end det man gerne vil ende med at lave (ibid). Det kunne for eksempel være hvis man skulle lave en taske, så ville man ikke lave prototypen af dyrt læder eller lignende (ibid). Det gør også at man har nemmere ved at se kritisk på sit arbejde så man kan forbedre ens prototype, så man ikke bliver utilfreds med sit endelige produkt (ibid).
+                </p>
+            </section> <!-- Skitser -->
+            
+            <section class="testmetoder-table">
+                <h2 class="overskrift">Testmetoder</h2>
+                <table class="usability-table">
+                    <tr>
+                        <th>Typisk fokusområde</th>
+                        <th>Særligt egnede testmetoder</th>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Navigation og struktur (informationsarkitektur)
+                        </td>
+                        <td>
+                            <p>Gangstertest (navigation)</p> 
+                            <p>Kortsortering (Informationsstruktur)</p> 
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Funktionalitet (interaktionsdesign)
+                        </td>
+                        <td>
+                            <p>Spørgeskema</p>
+                            <p>Ekspertvurdering</p>
+                            <p>Burgertest</p>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Layout og opbygning
+                        </td>
+                        <td>
+                            <p>Gangstertest</p>
+                            <p>Ekspertvurdering</p>
+                            <p>Brugertest</p>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Tilgængelighed
+                        </td>
+                        <td>
+                            <p>Ekspertvurdering</p>
+                            <p>Brugertest</p>
+                            <p>Tilgængelighedstest</p>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            Forståelighed
+                        </td>
+                        <td>
+                            <p>Spørgeskema </p>
+                            <p>Kortsortering</p>
+                            <p>Brugertest</p>
+                        </td>
+                    </tr>
+                </table>
+            </section> <!-- testmetoder -->
+            
+            <section class="kilder">
+                <h2 class="overskrift">Kilder</h2>
+                <ul class="kildeliste">
+                    <li>Gregersen, O. &#38; Wisler-Poulsen, I., 2017. Hvad er Usability?. I: Usability - Testmetoder til mere brugbare websites. s.l.:Wislers Forlag, pp. 14-24.</li>
+                    <li>Gregersen, O. &#38; Wisler-Poulsen, I., 2017. Undersøgelsesdesign. I: Usability - Testmetoder til mere brugbare websites. s.l.:Wislers Forlag, pp. 24-42.</li>
+                </ul>
+            </section>
+        </div>
+        
+        <?php include 'footer.inc' ?> <!-- footer -->
         
         <!-- Bootstrap script -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
